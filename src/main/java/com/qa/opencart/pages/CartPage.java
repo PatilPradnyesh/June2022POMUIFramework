c5acf95e99c549a2ca9c9f7ebd709209e071fe75
PatilPradnyesh <kpatil1240@gmail.com>
2022-10-26 09:15:46 +0530

    cart page updated as per naveen's review

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,8 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
 	String product = "iMac";
 	
+	By locator = By.className(product);;
+	
+	public void click() {
+		
+	}
 	
 }
